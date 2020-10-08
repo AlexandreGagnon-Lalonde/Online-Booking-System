3f7103dedaf726ba0c2638b5b013156df1fb71b6
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-10-08 12:08:12 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require("body-parser");
+
+const PORT = 1234;
+
+express()
+  .use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, HEAD, GET, PUT, POST, DELETE"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  })
+  .use(morgan("tiny"))
+  .use(express.static("./server/assets"))
+  .use(bodyParser.json())
+  .use(express.urlencoded({ extended: false }))
+  .use("/", express.static(__dirname + "/"))
+
+  // REST endpoints?
+  .get('/', (req,res) => res.send('wazza'))
+
+  .listen(PORT, () => console.info(`Listening on port ${PORT}`));
