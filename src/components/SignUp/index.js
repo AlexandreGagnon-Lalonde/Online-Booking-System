e028faca2c7325fa72220da448219c5ce17bec2e
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-10-11 16:57:25 -0400

    basic link to go leave sign up / login

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+
+import Header from '../Header/index';
 import SignUpForm from './SignUpForm';
 
 const SignUp = () => {
   return (
-    <SignUpForm />
+    <>
+      <Header />
+      <SignUpForm />
+    </>
   )
 }
 
