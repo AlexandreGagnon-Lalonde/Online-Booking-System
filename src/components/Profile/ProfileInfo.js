ad371652a03e881843abf5f9eb2eaa9e514f68a4
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-10-16 21:18:11 -0400

    profile info

--- src/components/Profile/ProfileInfo.js
+++ src/components/Profile/ProfileInfo.js
@@ -1,10 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 
+import { useSelector, useDispatch } from "react-redux";
+
 const ProfileInfo = () => {
+  const userState = useSelector((state) => state.user.user);
+  const otherUserState = useSelector((state) => state.user.otherUser)
+
   return (
-    <div>Mmmhh fresh info</div>
+    <StyledDiv>
+      <p>{userState.firstName + ' ' + userState.lastName}</p>
+      <p>{userState.phone}</p>
+      <p>{userState.DOB}</p>
+      <p>{userState.address + ' ' + userState.city}</p>
+      <p>{userState.gender}</p>
+    </StyledDiv>
   )
 }
+
+const StyledDiv = styled.div`
+  border: 1px solid black;
+  padding 10px;
+`
 
 export default ProfileInfo
