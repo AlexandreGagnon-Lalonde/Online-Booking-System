df15d50666b348460c5d67f6b4b50287ab972179
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-10-24 21:14:35 -0400

    click event but no content

--- src/components/HomePage/Calendar.js
+++ src/components/HomePage/Calendar.js
@@ -5,23 +5,35 @@ import moment from "moment";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
-
-import "react-week-calendar/dist/style.css";
+import interactionPlugin from "@fullcalendar/interaction";
 
 const Calendar = () => {
   const currentUser = useSelector((state) => state.user.user);
 
-  // Exclude times, filter times, include times,
-
-  // numberOfDays for mobile version
+// timeGridDay timeGridWeek dayGridMonth
   return (
     <>
       <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin]}
-        initialView={"timeGridDay"}
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView={"dayGridMonth"}
+        dateClick={handleClick}
+        eventRender={renderEventContent}
       />
     </>
   );
 };
+
+const handleClick = (arg) => {
+  alert(arg.dateStr)
+}
+
+const renderEventContent = (eventInfo) => {
+  return (
+    <>
+      <b>{eventInfo}</b>
+      <i>{'class'}</i>
+    </>
+  )
+}
 
 export default Calendar;
