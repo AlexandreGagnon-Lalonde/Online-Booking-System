294529b8fe5ab81d3cb3b13b514e95f9e2a21c58
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-11-02 13:59:33 -0500

    colors for design in constant

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,11 @@
 export const SERVER_URL = 'http://localhost:1234';
 
 export const CLIENT_URL = 'http://localhost:3000';
+
+export const COLORS = {
+  beige: '#fffcf2',
+  lightGray: '#ccc5b9',
+  mediumGray: '#403d39',
+  darkGray: '#252422',
+  orange: '#eb5e28',
+}
