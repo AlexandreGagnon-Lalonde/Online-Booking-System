eeb3ae29cc2d57269603971b3330cdb830ebfca6
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-11-07 09:13:25 -0500

    classe styles

--- src/components/Profile/IndividualClasses.js
+++ src/components/Profile/IndividualClasses.js
@@ -1,12 +1,23 @@
 import React from "react";
 import styled from "styled-components";
+import { COLORS } from '../../constant'
 
 const IndividualClasses = ({ classe }) => {
   return (
-    <div>
+    <IndiClassContainer>
       <p>{classe}</p>
-    </div>
+      <p>date</p>
+      <p>workout</p>
+    </IndiClassContainer>
   );
 };
+
+const IndiClassContainer = styled.div`
+  background-color: ${COLORS.lightGray};
+  border-radius: 5px;
+  margin: 10px;
+  color: ${COLORS.darkGray};
+  padding: 5px;
+`
 
 export default IndividualClasses;
--- src/components/Profile/ProfileClasses.js
+++ src/components/Profile/ProfileClasses.js
@@ -1,24 +1,44 @@
 import React from "react";
 import styled from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
-
+import { COLORS } from "../../constant";
 import IndividualClasses from "./IndividualClasses";
 
 const ProfileClasses = ({ user }) => {
   return (
-    <div>
+    <ClassesContainer>
       {user.classes.length > 0 ? (
-        <p>
-          Classes
-          {user.classes.map((classe) => {
-            return <IndividualClasses classe={classe} />;
-          })}
-        </p>
+        <>
+          <ClassesTitle>Classes</ClassesTitle>
+          <IndividualClassesContainer>
+            {user.classes.map((classe) => {
+              return <IndividualClasses classe={classe} />;
+            })}
+          </IndividualClassesContainer>
+        </>
       ) : (
-        <p>No Classes</p>
+        <ClassesTitle>No Classes</ClassesTitle>
       )}
-    </div>
+    </ClassesContainer>
   );
 };
+
+const ClassesContainer = styled.div`
+border-radius: 5px;
+background-color: ${COLORS.mediumGray};
+padding 10px;
+margin: 10px 50px;
+`;
+const ClassesTitle = styled.h2`
+  font-weight: bold;
+  font-size: 1.5em;
+  color: ${COLORS.orange};
+`;
+const IndividualClassesContainer = styled.div`
+display: grid;
+align-items: center;
+grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+
+`
 
 export default ProfileClasses;
